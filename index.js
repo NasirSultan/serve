9070ccfb69f7b65a21d20f7d6bd28d5f498a0395
NasirSultan <rainasirsultan123@gmail.com>
2025-04-27 14:23:40 +0500

    f

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cors({
   origin: [
     "https://youngdiv-frontend.vercel.app",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://your-frontend-domain.com"
   ],
   methods: ["POST", "DELETE", "GET", "PUT"],
   credentials: true
